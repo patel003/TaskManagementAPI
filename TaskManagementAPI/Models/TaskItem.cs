d043dfd3d450b81039eac3e3b2f9c8ed928107ef
Vikas patel <vp822156@gmail.com>
2026-02-12 23:43:30 +0530

    Added Models For User And Task Item

---
+++ TaskManagementAPI/Models/TaskItem.cs
@@ -0,0 +1,19 @@
+﻿namespace TaskManagementAPI.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } =   string.Empty;
+        public bool IsCompleted { get; set; }  = false;
+        public int Priority { get; set; } = 1;
+        public DateTime DueDate { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // Foreign key to User
+        public int UserId { get; set; }
+
+        // Navigation property to User
+        public User User { get; set; } = null!;
+    }
+}
